a02cd177395ebb22e68293d0bf241dbdff56ed13
Student <example@example.com>
2019-10-29 11:05:39 -0700

    clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -5,30 +5,16 @@ $(document).ready(function() {
     var userInput = $('#text-box').val(); //This is what the user input.
     var forLoopFunction = function(userInput){
       for (var i = 0; i < userInput.length; i++) {
-        if (userInput[i].match(/[aeiou]/gi))
-        // === ("a" || "i" || "e" || "o" || "u" || "A" || "E" || "I" || "O" || "U"))
-        {
+        if (userInput[i].match(/[aeiou]/gi)) {
           newArrayNoVowelsFor += "-";
         }
         else {
           newArrayNoVowelsFor += userInput.charAt(i);
-          console.log(userInput[i]);
         }
       }
       return(newArrayNoVowelsFor);
     }
 
     $("p").append("for loop result: " + forLoopFunction(userInput));
-    //
-    // var newNewArrayNoVowelsMap = userInput.split('');
-    // console.log(newNewArrayNoVowelsMap);
-
-
-    // for loop(for the length of user input) {
-    //   if consonant, add chr to newArrayNoVowels
-    //   if vowel, add - to newArrayNoVowels
-    //   return newArrayNoVowels
-    // }
-    // print newArrayNoVowels to p
   });
 });
